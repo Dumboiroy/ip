733ef373ad0f88e121df7993eb56d8002c9ae25b
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-20 20:24:53 +0800

    new task classes

---
+++ src/main/java/DeadlineTask.java
@@ -0,0 +1,13 @@
+public class DeadlineTask extends Task {
+    private String dateDue;
+
+    public DeadlineTask(String name, String dateDue) {
+        super(name);
+        this.dateDue = dateDue;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + dateDue + ")";
+    }
+}
