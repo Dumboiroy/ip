30a8193196fcd5ef4b538ea89482feb9ff43bfcf
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-20 13:14:23 +0800

    Renaming Duke to Seeyes

---
+++ src/main/java/Seeyes.java
@@ -0,0 +1,14 @@
+public class Seeyes {
+    public static String divider = "============================================================";
+
+    public static void printDivider() {
+        System.out.println(divider);
+    }
+    public static void main(String[] args) {
+        printDivider();
+        System.out.println("Hello, I'm Seeyes!\n" + "What can I do for you?");
+        printDivider();
+        System.out.println("Bye. Hope to see you again soon!");
+        printDivider();
+    }
+}
