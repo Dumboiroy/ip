0b8b0a8adabb46f9042db667107777050dc4d38d
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-20 18:24:44 +0800

    Added Task class

--- src/main/java/Seeyes.java
+++ src/main/java/Seeyes.java
@@ -2,10 +2,36 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Seeyes {
     public static String divider = "============================================================";
-    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<Task> list = new ArrayList<>();
 
     public static void printDivider() {
         System.out.println(divider);
+    }
+
+    public static void handleUserInput(String input) {
+        if (input.equals("list")) {
+            printList();
+        } else if (input.startsWith("mark")) {
+            
+        } else {
+            addToList(input);
+        }
+    }
+
+    public static void printList() {
+        printDivider();
+        System.out.println("Here are the items in your list:");
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) != null) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+        }
+    }
+
+    public static void addToList(String name) {
+        Task item = new Task(name);
+        list.add(item);
+        System.out.println("Added: " + item);
     }
 
     public static void main(String[] args) {
@@ -15,29 +41,17 @@ public class Seeyes {
         printDivider();
         while (true) {
             String userInput = scanner.nextLine();
+            handleUserInput(userInput);
             if (userInput.equals("bye")) {
                 break;
             }
-            if (userInput.equals("list")) {
-                printDivider();
-                System.out.println("Here are the items in your list:");
-                for (int i = 0; i < list.size(); i++) {
-                    if (list.get(i) != null) {
-                        System.out.println((i + 1) + ". " + list.get(i));
-                    }
-                }
-                printDivider();
-                continue;
-            }
-            printDivider();
-            list.add(userInput);
-            System.out.println("Added: " + userInput);
             printDivider();
         }
         printDivider();
         System.out.println("Bye. Hope to see you again soon!");
         printDivider();
-        
 
+        // Terminate
+        scanner.close();
     }
 }
