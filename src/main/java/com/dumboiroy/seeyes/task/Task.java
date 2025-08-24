291a3e9c86b426b577e4842c0fa07e3861d659bf
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-24 15:08:44 +0800

    Remove isDone() from Task

--- src/main/java/com/dumboiroy/seeyes/task/Task.java
+++ src/main/java/com/dumboiroy/seeyes/task/Task.java
@@ -9,10 +9,6 @@ public abstract class Task {
         isDone = false;
     }
 
-    public boolean isDone() {
-        return isDone;
-    }
-
     public void markAsDone() {
         isDone = true;
     }
