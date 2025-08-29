4e81d76aa9163900869494b840da5c33e3723671
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-29 13:37:24 +0800

    Add scripts and update tests

---
+++ text-ui-test/testoutput.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# create bin directory if it doesn't exist
+if [ ! -d "../bin" ]
+then
+    mkdir ../bin
+fi
+
+# delete output from previous run
+if [ -e "./ACTUAL.TXT" ]
+then
+    rm ACTUAL.TXT
+fi
+
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/com/dumboiroy/seeyes/*.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin com.dumboiroy.seeyes.Seeyes < input.txt > ACTUAL.TXT 2>&1
+
+# show the output
+cat ACTUAL.TXT
