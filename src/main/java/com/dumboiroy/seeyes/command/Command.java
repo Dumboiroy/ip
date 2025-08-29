9de746840727c9357d195c45caa6635a1cdf84c9
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-29 18:49:21 +0800

    Remove result from setData

--- src/main/java/com/dumboiroy/seeyes/command/Command.java
+++ src/main/java/com/dumboiroy/seeyes/command/Command.java
@@ -9,9 +9,8 @@ public abstract class Command {
     protected TaskList taskList;
     protected List<? extends Task> resultTasks;
 
-    public void setData(TaskList taskList, List<? extends Task> resultTasks) {
+    public void setData(TaskList taskList) {
         this.taskList = taskList;
-        this.resultTasks = resultTasks;
     }
 
     public abstract CommandResult execute();
