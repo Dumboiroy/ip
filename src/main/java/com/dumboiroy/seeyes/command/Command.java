dfc3a9a63b72e1dd6bb1bc330dd3feea5e4c4418
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-30 00:11:16 +0800

    Include storage in commands

--- src/main/java/com/dumboiroy/seeyes/command/Command.java
+++ src/main/java/com/dumboiroy/seeyes/command/Command.java
@@ -2,15 +2,18 @@ package com.dumboiroy.seeyes.command;
 
 import java.util.List;
 
+import com.dumboiroy.seeyes.storage.Storage;
 import com.dumboiroy.seeyes.task.Task;
 import com.dumboiroy.seeyes.task.TaskList;
 
 public abstract class Command {
     protected TaskList taskList;
+    protected Storage storage;
     protected List<? extends Task> resultTasks;
 
-    public Command setData(TaskList taskList) {
+    public Command setData(TaskList taskList, Storage storage) {
         this.taskList = taskList;
+        this.storage = storage;
         return this;
     }
 
