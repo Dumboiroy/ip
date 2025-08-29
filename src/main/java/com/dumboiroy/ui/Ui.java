5cc25992f63d71c8f7e57f14adb07bd0983cd193
Dumboiroy <alroylohzenyang@gmail.com>
2025-08-29 12:54:35 +0800

    Add list view

--- src/main/java/com/dumboiroy/ui/Ui.java
+++ src/main/java/com/dumboiroy/ui/Ui.java
@@ -13,7 +13,9 @@ public class Ui {
 
     private static final String DIVIDER = "============================================================";
     private static final String LS = System.lineSeparator();
-    private static final String LINE_PREFIX = ">> ";
+    private static final String SAY_LINE_PREFIX = ">> ";
+    private static final String PRINT_LINE_PREFIX = "## ";
+    private static final String FORMAT_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
     private final Scanner in;
     private final PrintStream out;
 
@@ -22,7 +24,7 @@ public class Ui {
         this.out = out;
     }
 
-    public static Ui newUi() {
+    public static Ui getUi() {
         return new Ui(System.in, System.out);
     }
 
@@ -35,7 +37,7 @@ public class Ui {
     }
 
     public String getNextUserCommand() {
-        out.print(LINE_PREFIX + "Enter a command:\n");
+        out.print(SAY_LINE_PREFIX + "Enter a command:\n");
         String rawInputLine = in.nextLine();
         while (shouldIgnore(rawInputLine)) {
             rawInputLine = in.nextLine();
@@ -43,43 +45,50 @@ public class Ui {
         return rawInputLine;
     }
 
-    public void show(String... message) {
+    public void say(String... message) {
         for (String m : message) {
-            out.println(LINE_PREFIX + m.replace("\n", LS + LINE_PREFIX));
+            out.println(SAY_LINE_PREFIX + m.replace("\n", LS + SAY_LINE_PREFIX));
+        }
+    }
+
+    public void print(String... message) {
+        for (String m : message) {
+            out.println(SAY_LINE_PREFIX + m.replace("\n", LS + PRINT_LINE_PREFIX));
         }
     }
 
     public void showError(String errorMessage) {
-        out.println(LINE_PREFIX + "ERROR: " + errorMessage);
+        out.println(SAY_LINE_PREFIX + "ERROR: " + errorMessage);
     }
 
-    // public void showResult(CommandResult result) {
-    // final Optional<List<? extends Task>> resultTasks = result.getResultTasks();
-    // if (resultTasks.isPresent()) {
-    // showResultTasks(resultTasks.get());
-    // }
-    // show(result.message, DIVIDER);
-    // }
+    public void showResult(CommandResult result) {
+        final Optional<List<? extends Task>> resultTasks = result.getResultTasks();
+        if (resultTasks.isPresent()) {
+            showResultTasks(resultTasks.get());
+        }
+        show(result.message, DIVIDER);
+    }
 
-    // private void showResultTasks(List<? extends Task> resultTasks) {
-    // final List<String> formattedTasks = new ArrayList<>();
-    // for (Task task : resultTasks) {
-    // formattedTasks.add(task.toString());
-    // }
-    // showAsIndexedList(formattedTasks);
-    // }
+    private void showResultTasks(List<? extends Task> tasks) {
+        showAsIndexedList(tasks.stream().map(task -> task.toString()).toList());
+    }
 
-    // private void showAsIndexedList(List<String> list) {
-    // showToUser(getIndexedListView(list));
-    // }
+    private void showAsIndexedList(List<String> list) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 1;
+        for (String item : list) {
+            formatted.append(getIndexedListItem(displayIndex, item)).append("\n");
+            displayIndex++;
+        }
+        print(formatted.toString());
+    }
 
-    // private static String getIndexedListView(List<String> list) {
-    // final StringBuilder formatted = new StringBuilder();
-
-    // }
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(FORMAT_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
 
     public void showWelcomeMessage() {
-        show(
+        say(
                 DIVIDER,
                 "Yo, I'm Seeyes!",
                 "How can I help?",
@@ -87,13 +96,13 @@ public class Ui {
     }
 
     public void showFarewellMessage() {
-        show(
+        say(
                 "See you around bro!",
                 DIVIDER);
     }
 
     public static void main(String[] args) {
-        Ui ui = Ui.newUi();
-        ui.show("Hello", "Message 1", "bye");
+        Ui ui = Ui.getUi();
+        ui.say("Hello", "Message 1", "bye");
     }
 }
